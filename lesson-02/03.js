d49e92184a367b4d0abceee0fc1bec70916eda2b
AntonovEvgen <eantonoff86@gmail.com>
2025-06-30 14:05:29 +0300

    commit-02/03-1

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -5,5 +5,15 @@
  */
 
 const score = 70 // тестовое значение, можно изменять
-let grade
+let grade = "F" 
+if (score < 50) {   
+} else if (score < 70) {
+    grade = "D"
+} else if ( score < 80) {
+    grade = "C"
+} else if (score < 90) {
+    grade = "B"
+}else if (score <= 100) {
+    grade = "A"
+}
 // your code
