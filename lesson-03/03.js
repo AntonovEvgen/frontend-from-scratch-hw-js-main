1553d21b38241d982a26bc598866d89ab05f966a
AntonovEvgen <eantonoff86@gmail.com>
2025-07-07 21:50:37 +0300

    commit-03/03-1

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,12 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(number1, number2, number3) {
+    largest = number1
+    if (number2 > largest) {
+        largest = number2
+    } if (number3 > largest) {
+        largest = number3
+    }
+    return largest
+}
+
